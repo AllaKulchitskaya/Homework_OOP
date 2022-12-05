962e37edab7eebfac00f04e7d9bbf623718731db
AllaKulchitskaya <alla070@list.ru>
2022-12-05 12:30:23 +0300

    s

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        Person maxim = new Person("Максим", "Минск", 1988, "бренд-менеджер");
-        System.out.println(maxim);
-        Person anya = new Person("Аня", "Москва", 1993, "методист образовательных программ");
-        System.out.println(anya);
-        Person katya = new Person("Катя", "Калининград", 1992, "продакт-менеджер");
-        System.out.println(katya);
-        Person artem = new Person("Артём", "Москва", 1995, "директор по развитию бизнеса");
-        System.out.println(artem);
     }
 }
