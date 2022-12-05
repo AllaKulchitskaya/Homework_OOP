0dbc06fe2be3778946ea09c61f38e598567bc359
AllaKulchitskaya <alla070@list.ru>
2022-12-05 12:27:36 +0300

    Задание 2

--- src/Person.java
+++ src/Person.java
@@ -2,15 +2,17 @@ public class Person {
     private String name;
     private String town;
     private int yearOfBirth;
+    private String jobTitle;
 
-    public Person(String name, String town, int yearOfBirth) {
+    public Person(String name, String town, int yearOfBirth, String jobTitle) {
         this.name = name;
         this.town = town;
         this.yearOfBirth = yearOfBirth;
+        this.jobTitle = jobTitle;
     }
 
     public String toString() {
         return "Привет! Меня зовут " + this.name + ". Я из города " + this.town + ". Мой год рождения - " +
-                this.yearOfBirth + ". Будем знакомы!";
+                this.yearOfBirth + ". Я работаю на должности " + this.jobTitle + ". Будем знакомы!";
     }
 }
